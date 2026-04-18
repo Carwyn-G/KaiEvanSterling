6ce8cbb3bc6e39941cc48ec71fc8df192b1b3071
Carwyn-G <gica25fx@student.ju.se>
2026-04-18 16:34:18 +0200

    better image loading on home page, explore pages now mobile-friendly

--- script.js
+++ script.js
@@ -71,16 +71,7 @@ document.getElementById('maillist-form')
 });
 
 
-let ciwContents = `
-  <div class="cover-art">
-    <img class="cover" id="CIW-back" src="images/CIW/layer1-water.png" alt="a large lake">
-    <img class="cover" id="CIW-one" src="images/CIW/layer2-island-base.png" alt="a pale castle on an island">
-    <img class="cover" id="CIW-two" src="images/CIW/layer3-middle-castle.png" alt="a pale castle on an island">
-    <img class="cover" id="CIW-three" src="images/CIW/layer4-front-castle.png" alt="a pale castle on an island">
-    <img class="cover" id="CIW-front" src="images/CIW/layer5-surface-water.png" alt="a large lake">
-  </div>
-  <section class="bookshelf">
-    <div class="bookshelf-contents">
+let ciwSynopsis = `
       <h1 class="book-title">The Crown is Waiting</h1>
       <div class="synopsis">
         <p>When Felri wakes up underground, he’s forced to dig his way out of his own grave. Unable to go back to his old life, he scrambles to make sense of why he isn’t staying dead. He soon finds himself pushed into a dangerous job as a royal guard for the heir to the throne. He has no idea what he’s doing, the heir is nothing like he expected, and a sinister plot is unfolding in the castle.</p>
@@ -88,24 +79,13 @@ let ciwContents = `
       </div>
       <a href="#"><button type="button">Buy Now</button></a>
       <div class="book-selector">
-        <button onclick="selectBook(this.id)" class="selected" id="CIW"><img src="images/CIW/3D-book.png" alt="The hardcover book 'the Crown is Waiting'"></button>
-        <button onclick="selectBook(this.id)" class="selector" id="KIW"><img src="images/KIW/3D-book.png" alt="The hardcover book 'the Kingdom is Watching'"></button>
-        <button onclick="selectBook(this.id)" class="selector" id="MIF"><h2>?</h2></button>
+        <button onclick="selectBook(this.id)" class="selected" id="CIW"><img src="images/CIW/3D-book.png" alt="The hardcover book 'the Crown is Waiting'" class="selectimg"></button>
+        <button onclick="selectBook(this.id)" class="selector" id="KIW"><img src="images/KIW/3D-book.png" alt="The hardcover book 'the Kingdom is Watching'"  class="selectimg"></button>
+        <button onclick="selectBook(this.id)" class="selector" id="MIF"><h2  class="selectimg">?</h2></button>
       </div>
-    </div>
-  </section>
   `
 
-let kiwContents = `
-  <div class="cover-art">
-    <img class="cover" id="KIW-back" src="images/KIW/layer1-water.png" alt="a large lake">
-    <img class="cover" id="KIW-one" src="images/KIW/layer2-lakesurface.png" alt="a large lake">
-    <img class="cover" id="KIW-two" src="images/KIW/layer3-backcity.png" alt="a castle on an island connected by bridge to a large city of skyscrapers">
-    <img class="cover" id="KIW-three" src="images/KIW/layer4-midcity.png" alt="a large city of sky scrapers">
-    <img class="cover" id="KIW-front" src="images/KIW/layer5-frontcity.png" alt="a city of skyscrapers">
-  </div>
-  <section class="bookshelf">
-    <div class="bookshelf-contents">
+let kiwSynopsis = `
       <h1 class="book-title">The Kingdom is Watching</h1>
       <div class="synopsis">
         <p>Now engaged to the youngest Queen to ever rule Astuna and already world famous for his role as a guard, Felri finds himself thrust into a spotlight he never asked for, all while having to keep the engagement a secret from the whole country. With Sievert behind bars awaiting a trial for his role in the coronation disaster, Torin’s uncle is serving as King. But tensions are still running high in the castle and throughout the country.</p>
@@ -113,24 +93,13 @@ let kiwContents = `
       </div>
       <a href="buykiw.html" target="_blank"><button type="button">Buy Now</button></a>
       <div class="book-selector">
-        <button onclick="selectBook(this.id)" class="selector" id="CIW"><img src="images/CIW/3D-book.png" alt="The hardcover book 'the Crown is Waiting'"></button>
-        <button onclick="selectBook(this.id)" class="selected" id="KIW"><img src="images/KIW/3D-book.png" alt="The hardcover book 'the Kingdom is Watching'"></button>
-        <button onclick="selectBook(this.id)" class="selector" id="MIF"><h2>?</h2></button>
+        <button onclick="selectBook(this.id)" class="selector" id="CIW"><img src="images/CIW/3D-book.png" alt="The hardcover book 'the Crown is Waiting'" class="selectimg"></button>
+        <button onclick="selectBook(this.id)" class="selected" id="KIW"><img src="images/KIW/3D-book.png" alt="The hardcover book 'the Kingdom is Watching'"  class="selectimg"></button>
+        <button onclick="selectBook(this.id)" class="selector" id="MIF"><h2  class="selectimg">?</h2></button>
       </div>
-    </div>
-  </section>
   `
 
-let mifContents = `
-  <div class="cover-art" id="MIF-cover">
-    <img class="cover" id="MIF-back" src="images/MIF/layer1.png" alt="clouds">
-    <img class="cover" id="MIF-one" src="images/MIF/layer2.png" alt="clouds">
-    <img class="cover" id="MIF-two" src="images/MIF/layer3.png" alt="Coming Soon">
-    <img class="cover" id="MIF-three" src="images/MIF/layer4.png" alt="clouds">
-    <img class="cover" id="MIF-front" src="images/MIF/layer5.png" alt="clouds">
-  </div>
-  <section class="bookshelf">
-    <div class="bookshelf-contents">
+let mifSynopsis = `
       <h1 class="book-title">The Monarchy is Falling</h1>
       <div class="synopsis">
         <p>Felri's made it through the wedding. He thought that would be enough, but now that he's crowned King, the real challenges are only beginning.</p>
@@ -142,29 +111,62 @@ let mifContents = `
         </form>
       </div>
       <div class="book-selector">
-        <button onclick="selectBook(this.id)" class="selector" id="CIW"><img src="images/CIW/3D-book.png" alt="The hardcover book 'the Crown is Waiting'"></button>
-        <button onclick="selectBook(this.id)" class="selector" id="KIW"><img src="images/KIW/3D-book.png" alt="The hardcover book 'the Kingdom is Watching'"></button>
-        <button onclick="selectBook(this.id)" class="selected" id="MIF"><h2>?</h2></button>
+        <button onclick="selectBook(this.id)" class="selector" id="CIW"><img src="images/CIW/3D-book.png" alt="The hardcover book 'the Crown is Waiting'" class="selectimg"></button>
+        <button onclick="selectBook(this.id)" class="selector" id="KIW"><img src="images/KIW/3D-book.png" alt="The hardcover book 'the Kingdom is Watching'"  class="selectimg"></button>
+        <button onclick="selectBook(this.id)" class="selected" id="MIF"><h2  class="selectimg">?</h2></button>
       </div>
-    </div>
-  </section>
   `
 
 
 
+let shelfContents = document.getElementsByClassName("bookshelf-contents");
 let displayed = document.getElementsByClassName("book-display");
+let ciwCovers = document.getElementsByClassName("CIW");
+let kiwCovers = document.getElementsByClassName("KIW");
+let mifCovers = document.getElementsByClassName("MIF");
 
 function selectBook(z){
   console.log(z);
   if (z === "CIW"){
-    displayed[0].innerHTML = ciwContents;
-    displayed[0].id = "CIW-book"
-  } else if (z ==="KIW"){
-    displayed[0].innerHTML = kiwContents;
-    displayed[0].id = "KIW-book"
-  } else if (z ==="MIF"){
-    displayed[0].innerHTML = mifContents;
-    displayed[0].id = "MIF-book"
+    for (let i = 0; i < ciwCovers.length; i++) {
+      console.log(ciwCovers[i]);
+      ciwCovers[i].className="cover CIW";
+    }
+    for (let j = 0; j < kiwCovers.length; j++) {
+      kiwCovers[j].className="cover KIW hidden";
+    }
+    for (let k = 0; k < mifCovers.length; k++) {
+      mifCovers[k].className="cover MIF hidden";
+    }
+    shelfContents[0].innerHTML = ciwSynopsis;
+    displayed[0].id = "CIW-book";
+  } 
+  else if (z ==="KIW"){
+    for (let i = 0; i < ciwCovers.length; i++) {
+      console.log(ciwCovers[i]);
+      ciwCovers[i].className="cover CIW hidden";
+    }
+    for (let j = 0; j < kiwCovers.length; j++) {
+      kiwCovers[j].className="cover KIW";
+    }
+    for (let k = 0; k < mifCovers.length; k++) {
+      mifCovers[k].className="cover MIF hidden";
+    }
+    shelfContents[0].innerHTML = kiwSynopsis;
+    displayed[0].id = "KIW-book";
+  } 
+  else if (z ==="MIF"){
+    for (let i = 0; i < ciwCovers.length; i++) {
+      ciwCovers[i].className="cover CIW hidden";
+    }
+    for (let j = 0; j < kiwCovers.length; j++) {
+      kiwCovers[j].className="cover KIW hidden";
+    }
+    for (let k = 0; k < mifCovers.length; k++) {
+      mifCovers[k].className="cover MIF";
+    }
+    shelfContents[0].innerHTML = mifSynopsis;
+    displayed[0].id = "MIF-book";
   }
 }
 
